596e893e77f52196d30f751cba78cb138e567e09
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-02-02 13:17:59 +0100

    update

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((err) => console.error("Error detected: ", err));
+let data = '';
 
+let searchInput = document.getElementById('searchField');
+
+let searchResultTitle = document.getElementById('searchResultTitle');
+
+let searchResultsContainer = document.getElementById('searchResult');
+
+let search = () => {
+    let value = searchInput.value;
+    console.log(value);
+    searchInput.value = '';
+
+    searchResultTitle.classList.add('d-block');
+
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${value}`)
+        .then((response) => response.json())
+        .then((json) => {
+            data = json;
+            console.log(data);
+
+            searchResultsContainer.innerHTML = '';
+
+            data.data.forEach(item => {
+                let card = document.createElement('div');
+                card.classList.add('col-md-3', 'col-6', 'mb-4');
+
+                card.innerHTML = `
+                    <div class="card h-100 border-0 shadow-sm">
+                        <img src="${item.album.cover}" class="card-img-top rounded" alt="${item.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${item.title}</h5>
+                            <p class="card-text">${item.artist.name}</p>
+                        </div>
+                    </div>
+                `;
+
+                searchResultsContainer.appendChild(card);
+            });
+
+
+        })
+        .catch((err) => console.error("Error detected: ", err));
+}
